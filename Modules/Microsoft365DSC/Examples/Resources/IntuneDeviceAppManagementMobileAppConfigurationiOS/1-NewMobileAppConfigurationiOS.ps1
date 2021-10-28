43c916a30f66ca4f11ce3af997eb4ea883074bcf
headcra6 <57180030+headcra6@users.noreply.github.com>
2021-10-29 00:40:51 +0300

    Create 1-NewMobileAppConfigurationiOS.ps1

---
+++ Modules/Microsoft365DSC/Examples/Resources/IntuneDeviceAppManagementMobileAppConfigurationiOS/1-NewMobileAppConfigurationiOS.ps1
@@ -0,0 +1,24 @@
+<#
+This example creates a new App Configuration Policy.
+#>
+
+Configuration Example
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $credsGlobalAdmin
+    )
+    Import-DscResource -ModuleName Microsoft365DSC
+
+    node localhost
+    {
+        IntuneDeviceAppManagementMobileAppConfigurationiOS AddNewMobileAppConfigiOS
+        {
+            DisplayName          = 'Contoso'
+            Description          = 'Contoso Policy'
+            Ensure               = 'Present'
+            Credential           = $credsGlobalAdmin;
+        }
+    }
+}
